b1ab6d4a053935c1a3aacdf7284a38849678843a
Alejandro Garcia Arriaga <alejandro.g.arriaga98@gmail.com>
2021-05-19 00:21:30 -0500

    set libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "ADC_Read.h"
+#include <Kalman_Filter.h>
+#include <BasicLinearAlgebra.h>
 
 void setup() {
   // put your setup code here, to run once:
